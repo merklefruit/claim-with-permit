1fdb02ae1feeae9dfb5c6e0c1bbda84fb4479ea2
Nicolas <nicolas@racchi.com>
2022-12-18 15:37:43 +0100

    feat: added more tests

--- test/ClaimWithPermit.t.sol
+++ test/ClaimWithPermit.t.sol
@@ -28,7 +28,7 @@ contract ERC20Test is Test {
         sigUtils = new SigUtils(claimWithPermit.DOMAIN_SEPARATOR(), claimWithPermit.PERMIT_TYPEHASH());
     }
 
-    function testClaimWithPermit() public {
+    function test_SuccessfullyClaimWithPermit() public {
         SigUtils.Permit memory permit = SigUtils.Permit({spender: claimer, rewardId: 1, nonce: 0, deadline: 1 days});
 
         bytes32 digest = sigUtils.getTypedDataHash(permit);
@@ -40,5 +40,31 @@ contract ERC20Test is Test {
 
         assertEq(claimWithPermit.balanceOf(claimer), 1);
         assertEq(claimWithPermit.verifierNonce(), 1);
+    }
+
+    function testRevert_ExpiredPermit() public {
+        SigUtils.Permit memory permit = SigUtils.Permit({spender: claimer, rewardId: 1, nonce: 0, deadline: 1 days});
+
+        bytes32 digest = sigUtils.getTypedDataHash(permit);
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(verifierPrivateKey, digest);
+
+        vm.prank(claimer);
+        vm.warp(1 days + 1 seconds); // fast forward one second past the deadline
+        vm.expectRevert(ClaimWithPermit.PermitDeadlineExpired.selector);
+        claimWithPermit.claim(permit.rewardId, permit.deadline, v, r, s);
+    }
+
+    function testRevert_InvalidSignature() public {
+        SigUtils.Permit memory permit = SigUtils.Permit({spender: claimer, rewardId: 1, nonce: 0, deadline: 1 days});
+
+        bytes32 digest = sigUtils.getTypedDataHash(permit);
+
+        // sign the permit with the claimer's private key instead of the verifier's
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(claimerPrivateKey, digest);
+
+        vm.prank(claimer);
+        vm.expectRevert(ClaimWithPermit.InvalidSignature.selector);
+        claimWithPermit.claim(permit.rewardId, permit.deadline, v, r, s);
     }
 }
